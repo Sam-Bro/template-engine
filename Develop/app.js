e6daff102bed57a056da2952e3611b4ca2733f6b
OrbmasWin <sambrostudent@gmail.com>
2020-04-28 19:30:20 +0800

    added manager prompt function

--- Develop/app.js
+++ Develop/app.js
@@ -4,15 +4,78 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
 ​
 ​
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const teamMembers = [];
+const idArray = [];
+
+function teamApp() {
+    function createManager() {
+        console.log("Begin building your team");
+        inquirer.prompt([
+            {
+                type:"input",
+                name: "managerName",
+                message: "What is the managers name?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is the managers id?",
+                validate: answer => {
+                    const pass = answer.match(/^[1-9]\d*$/);
+                    if (pass) {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is the managers Email?",
+                validate: answer => {
+                    const pass = answer.match(/\S+@\S+\.\S+/);
+                    if (pass) {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "managerOfficeNo",
+                message: "What us the managers office number?",
+                validate: answer => {
+                    const pass = answer.match(/^[1-9]\d*$/);
+                    if (pass) {
+                        return true;
+                    }
+                }
+            }
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNo);
+            teamMembers.push(manager);
+            idArray.push(answers.managerId);
+            createTeam();
+        });
+    }
+
+    createManager();
+}
+
+teamApp()
+
 ​
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
